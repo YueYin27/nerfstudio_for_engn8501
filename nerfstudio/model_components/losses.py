modified loss & corrected distance map calculation

nerfstudio/model_components/losses.py
@@ -45,11 +45,11 @@ class DepthLossType(Enum):
 
 
 def outer(
-    t0_starts: Float[Tensor, "*batch num_samples_0"],
-    t0_ends: Float[Tensor, "*batch num_samples_0"],
-    t1_starts: Float[Tensor, "*batch num_samples_1"],
-    t1_ends: Float[Tensor, "*batch num_samples_1"],
-    y1: Float[Tensor, "*batch num_samples_1"],
+        t0_starts: Float[Tensor, "*batch num_samples_0"],
+        t0_ends: Float[Tensor, "*batch num_samples_0"],
+        t1_starts: Float[Tensor, "*batch num_samples_1"],
+        t1_ends: Float[Tensor, "*batch num_samples_1"],
+        y1: Float[Tensor, "*batch num_samples_1"],
 ) -> Float[Tensor, "*batch num_samples_0"]:
     """Faster version of
 
@@ -77,10 +77,10 @@ def outer(
 
 
 def lossfun_outer(
-    t: Float[Tensor, "*batch num_samples_1"],
-    w: Float[Tensor, "*batch num_samples"],
-    t_env: Float[Tensor, "*batch num_samples_1"],
-    w_env: Float[Tensor, "*batch num_samples"],
+        t: Float[Tensor, "*batch num_samples_1"],
+        w: Float[Tensor, "*batch num_samples"],
+        t_env: Float[Tensor, "*batch num_samples_1"],
+        w_env: Float[Tensor, "*batch num_samples"],
 ):
     """
     https://github.com/kakaobrain/NeRF-Factory/blob/f61bb8744a5cb4820a4d968fb3bfbed777550f4a/src/model/mipnerf360/helper.py#L136
@@ -135,7 +135,43 @@ def lossfun_distortion(t, w):
     dut = torch.abs(ut[..., :, None] - ut[..., None, :])
     loss_inter = torch.sum(w * torch.sum(w[..., None, :] * dut, dim=-1), dim=-1)
 
-    loss_intra = torch.sum(w**2 * (t[..., 1:] - t[..., :-1]), dim=-1) / 3
+    loss_intra = torch.sum(w ** 2 * (t[..., 1:] - t[..., :-1]), dim=-1) / 3
+
+    return loss_inter + loss_intra
+
+
+def lossfun_distortion_refractive(t, w, ray_samples):
+    """
+    https://github.com/kakaobrain/NeRF-Factory/blob/f61bb8744a5cb4820a4d968fb3bfbed777550f4a/src/model/mipnerf360/helper.py#L142
+    https://github.com/google-research/multinerf/blob/b02228160d3179300c7d499dca28cb9ca3677f32/internal/stepfun.py#L266
+    """
+    scale_factor, s_near, s_far = 0.1, 0.05, 14.0
+    ray_samples.get_refracted_rays()
+    intersection_0, intersection_1 = ray_samples.frustums.intersections  # get intersections
+    intersection_0 = (intersection_0 / scale_factor - s_near) / (s_far - s_near)
+    intersection_1 = (intersection_1 / scale_factor - s_near) / (s_far - s_near)
+    origins = (ray_samples.frustums.origins / scale_factor - s_near) / (s_far - s_near)
+    intersection_distance_0 = torch.norm(intersection_0 - origins, dim=-1)
+    intersection_distance_1 = torch.norm(intersection_1 - origins, dim=-1)
+
+    ut = (t[..., 1:] + t[..., :-1]) / 2  # midpoints between each start/end, i.e. sample points
+
+    if not torch.isnan(intersection_distance_0[0][0]):
+        mask = (ut < intersection_distance_0) | (ut > intersection_distance_1)  # create a mask to exclude points
+        ut = torch.where(mask, ut, torch.zeros_like(ut))
+        w = torch.where(mask, w, torch.zeros_like(w))
+        dut = torch.abs(ut[..., :, None] - ut[..., None, :])  # distances between all pairs of sample points
+        zero_positions = (ut == 0.0)  # find the positions where each row in ut is 0.0
+        mask_per_row = zero_positions.unsqueeze(1).expand(-1, dut.size(2), -1)  # create masks for each row
+        dut[mask_per_row] = 0.0  # set the corresponding distances in dut to zero
+        t_new = (t[..., 1:] - t[..., :-1])
+        t_new = torch.where(mask, t_new, torch.zeros_like(t_new))
+    else:
+        dut = torch.abs(ut[..., :, None] - ut[..., None, :])  # distances between all pairs of sample points
+        t_new = (t[..., 1:] - t[..., :-1])
+
+    loss_inter = torch.sum(w * torch.sum(w[..., None, :] * dut, dim=-1), dim=-1)
+    loss_intra = torch.sum(w ** 2 * t_new, dim=-1) / 3
 
     return loss_inter + loss_intra
 
@@ -149,9 +185,9 @@ def distortion_loss(weights_list, ray_samples_list):
 
 
 def nerfstudio_distortion_loss(
-    ray_samples: RaySamples,
-    densities: Optional[Float[Tensor, "*bs num_samples 1"]] = None,
-    weights: Optional[Float[Tensor, "*bs num_samples 1"]] = None,
+        ray_samples: RaySamples,
+        densities: Optional[Float[Tensor, "*bs num_samples 1"]] = None,
+        weights: Optional[Float[Tensor, "*bs num_samples 1"]] = None,
 ) -> Float[Tensor, "*bs 1"]:
     """Ray based distortion loss proposed in MipNeRF-360. Returns distortion Loss.
 
@@ -184,18 +220,18 @@ def nerfstudio_distortion_loss(
     midpoints = (starts + ends) / 2.0  # (..., num_samples, 1)
 
     loss = (
-        weights * weights[..., None, :, 0] * torch.abs(midpoints - midpoints[..., None, :, 0])
+            weights * weights[..., None, :, 0] * torch.abs(midpoints - midpoints[..., None, :, 0])
     )  # (..., num_samples, num_samples)
     loss = torch.sum(loss, dim=(-1, -2))[..., None]  # (..., num_samples)
-    loss = loss + 1 / 3.0 * torch.sum(weights**2 * (ends - starts), dim=-2)
+    loss = loss + 1 / 3.0 * torch.sum(weights ** 2 * (ends - starts), dim=-2)
 
     return loss
 
 
 def orientation_loss(
-    weights: Float[Tensor, "*bs num_samples 1"],
-    normals: Float[Tensor, "*bs num_samples 3"],
-    viewdirs: Float[Tensor, "*bs 3"],
+        weights: Float[Tensor, "*bs num_samples 1"],
+        normals: Float[Tensor, "*bs num_samples 3"],
+        viewdirs: Float[Tensor, "*bs 3"],
 ):
     """Orientation loss proposed in Ref-NeRF.
     Loss that encourages that all visible normals are facing towards the camera.
@@ -208,20 +244,20 @@ def orientation_loss(
 
 
 def pred_normal_loss(
-    weights: Float[Tensor, "*bs num_samples 1"],
-    normals: Float[Tensor, "*bs num_samples 3"],
-    pred_normals: Float[Tensor, "*bs num_samples 3"],
+        weights: Float[Tensor, "*bs num_samples 1"],
+        normals: Float[Tensor, "*bs num_samples 3"],
+        pred_normals: Float[Tensor, "*bs num_samples 3"],
 ):
     """Loss between normals calculated from density and normals from prediction network."""
     return (weights[..., 0] * (1.0 - torch.sum(normals * pred_normals, dim=-1))).sum(dim=-1)
 
 
 def ds_nerf_depth_loss(
-    weights: Float[Tensor, "*batch num_samples 1"],
-    termination_depth: Float[Tensor, "*batch 1"],
-    steps: Float[Tensor, "*batch num_samples 1"],
-    lengths: Float[Tensor, "*batch num_samples 1"],
-    sigma: Float[Tensor, "0"],
+        weights: Float[Tensor, "*batch num_samples 1"],
+        termination_depth: Float[Tensor, "*batch 1"],
+        steps: Float[Tensor, "*batch num_samples 1"],
+        lengths: Float[Tensor, "*batch num_samples 1"],
+        sigma: Float[Tensor, "0"],
 ) -> Float[Tensor, "*batch 1"]:
     """Depth loss from Depth-supervised NeRF (Deng et al., 2022).
 
@@ -242,11 +278,11 @@ def ds_nerf_depth_loss(
 
 
 def urban_radiance_field_depth_loss(
-    weights: Float[Tensor, "*batch num_samples 1"],
-    termination_depth: Float[Tensor, "*batch 1"],
-    predicted_depth: Float[Tensor, "*batch 1"],
-    steps: Float[Tensor, "*batch num_samples 1"],
-    sigma: Float[Tensor, "0"],
+        weights: Float[Tensor, "*batch num_samples 1"],
+        termination_depth: Float[Tensor, "*batch 1"],
+        predicted_depth: Float[Tensor, "*batch 1"],
+        steps: Float[Tensor, "*batch num_samples 1"],
+        sigma: Float[Tensor, "0"],
 ) -> Float[Tensor, "*batch 1"]:
     """Lidar losses from Urban Radiance Fields (Rematas et al., 2022).
 
@@ -273,7 +309,7 @@ def urban_radiance_field_depth_loss(
     line_of_sight_loss_near = (weights - torch.exp(target_distribution.log_prob(steps - termination_depth))) ** 2
     line_of_sight_loss_near = (line_of_sight_loss_near_mask * line_of_sight_loss_near).sum(-2)
     line_of_sight_loss_empty_mask = steps < termination_depth - sigma
-    line_of_sight_loss_empty = (line_of_sight_loss_empty_mask * weights**2).sum(-2)
+    line_of_sight_loss_empty = (line_of_sight_loss_empty_mask * weights ** 2).sum(-2)
     line_of_sight_loss = line_of_sight_loss_near + line_of_sight_loss_empty
 
     loss = (expected_depth_loss + line_of_sight_loss) * depth_mask
@@ -281,14 +317,14 @@ def urban_radiance_field_depth_loss(
 
 
 def depth_loss(
-    weights: Float[Tensor, "*batch num_samples 1"],
-    ray_samples: RaySamples,
-    termination_depth: Float[Tensor, "*batch 1"],
-    predicted_depth: Float[Tensor, "*batch 1"],
-    sigma: Float[Tensor, "0"],
-    directions_norm: Float[Tensor, "*batch 1"],
-    is_euclidean: bool,
-    depth_loss_type: DepthLossType,
+        weights: Float[Tensor, "*batch num_samples 1"],
+        ray_samples: RaySamples,
+        termination_depth: Float[Tensor, "*batch 1"],
+        predicted_depth: Float[Tensor, "*batch 1"],
+        sigma: Float[Tensor, "0"],
+        directions_norm: Float[Tensor, "*batch 1"],
+        is_euclidean: bool,
+        depth_loss_type: DepthLossType,
 ) -> Float[Tensor, "0"]:
     """Implementation of depth losses.
 
@@ -320,7 +356,7 @@ def depth_loss(
 
 
 def monosdf_normal_loss(
-    normal_pred: Float[Tensor, "num_samples 3"], normal_gt: Float[Tensor, "num_samples 3"]
+        normal_pred: Float[Tensor, "num_samples 3"], normal_gt: Float[Tensor, "num_samples 3"]
 ) -> Float[Tensor, "0"]:
     """
     Normal consistency loss proposed in monosdf - https://niujinshuchong.github.io/monosdf/
@@ -350,10 +386,10 @@ class MiDaSMSELoss(nn.Module):
         self.mse_loss = MSELoss(reduction="none")
 
     def forward(
-        self,
-        prediction: Float[Tensor, "1 32 mult"],
-        target: Float[Tensor, "1 32 mult"],
-        mask: Bool[Tensor, "1 32 mult"],
+            self,
+            prediction: Float[Tensor, "1 32 mult"],
+            target: Float[Tensor, "1 32 mult"],
+            mask: Bool[Tensor, "1 32 mult"],
     ) -> Float[Tensor, "0"]:
         """
         Args:
@@ -390,10 +426,10 @@ class GradientLoss(nn.Module):
         self.__scales = scales
 
     def forward(
-        self,
-        prediction: Float[Tensor, "1 32 mult"],
-        target: Float[Tensor, "1 32 mult"],
-        mask: Bool[Tensor, "1 32 mult"],
+            self,
+            prediction: Float[Tensor, "1 32 mult"],
+            target: Float[Tensor, "1 32 mult"],
+            mask: Bool[Tensor, "1 32 mult"],
     ) -> Float[Tensor, "0"]:
         """
         Args:
@@ -420,10 +456,10 @@ class GradientLoss(nn.Module):
         return total
 
     def gradient_loss(
-        self,
-        prediction: Float[Tensor, "1 32 mult"],
-        target: Float[Tensor, "1 32 mult"],
-        mask: Bool[Tensor, "1 32 mult"],
+            self,
+            prediction: Float[Tensor, "1 32 mult"],
+            target: Float[Tensor, "1 32 mult"],
+            mask: Bool[Tensor, "1 32 mult"],
     ) -> Float[Tensor, "0"]:
         """
         multiscale, scale-invariant gradient matching term to the disparity space.
@@ -476,10 +512,10 @@ class ScaleAndShiftInvariantLoss(nn.Module):
         self.__prediction_ssi = None
 
     def forward(
-        self,
-        prediction: Float[Tensor, "1 32 mult"],
-        target: Float[Tensor, "1 32 mult"],
-        mask: Bool[Tensor, "1 32 mult"],
+            self,
+            prediction: Float[Tensor, "1 32 mult"],
+            target: Float[Tensor, "1 32 mult"],
+            mask: Bool[Tensor, "1 32 mult"],
     ) -> Float[Tensor, "0"]:
         """
         Args:
@@ -542,8 +578,8 @@ class _GradientScaler(torch.autograd.Function):  # typing: ignore
 
 
 def scale_gradients_by_distance_squared(
-    field_outputs: Dict[FieldHeadNames, torch.Tensor],
-    ray_samples: RaySamples,
+        field_outputs: Dict[FieldHeadNames, torch.Tensor],
+        ray_samples: RaySamples,
 ) -> Dict[FieldHeadNames, torch.Tensor]:
     """
     Scale gradients by the ray distance to the pixel

nerfstudio/pipelines/base_pipeline.py
@@ -424,44 +424,49 @@ class VanillaPipeline(Pipeline):
 
                 # save depth map (replace transform_train/val.json to transform_test.json to get res on train/val set)
                 # new_max = 5.0
+                # TODO:
                 new_max = 13.15
                 new_min = 3.0
+                old_max = 1
+                old_min = 0
+                scale_factor = 0.1
 
-                # convert gt depth map back to world coordinate
-                ground_truth_depth = batch["depth_image"]
-                depth_mask = ground_truth_depth > 0  # DON'T PUT IT AFTER THE NEXT LINE...
-                ground_truth_depth = (ground_truth_depth-0.0) * (new_min-new_max) / (1.0-0.0) + new_max
-                ground_truth_depth[~depth_mask] = new_max
+                # convert gt depth map back to world coordinate and then to distance map
+                depth_gt = batch["depth_image"] / 0.0001 / 255.0  # ranging from 0 to 1
+                depth_gt_world = new_min + (new_max - new_min) * (old_max - depth_gt) / (old_max - old_min)
+                # print(depth_gt_world.max(), depth_gt_world.min())
+                distance_gt = depth_gt_world * camera_ray_bundle.metadata["directions_norm"]  # depth -> distance
+                # print(distance_gt.max(), distance_gt.min())
 
-                predicted_depth = outputs["depth"].to(torch.float64)
+                # convert predicted distance map to world coordinate
+                distance_pred = outputs["depth"].to(torch.float64)
+                distance_pred /= scale_factor
 
-                # perform (a*d + b) to predicted depth map to make sure gt and pred are in the same scale
-                # gt = torch.cat((gt, ground_truth_depth[depth_mask].cpu()), dim=0)  # ##########################
-                # pred = torch.cat((pred, predicted_depth[depth_mask].cpu()), dim=0)  # ##########################
-                # print(gt.shape, pred.shape)  # ##########################
-                # ones = torch.ones(len(predicted_depth[depth_mask]), 1).to('cuda:0')
-                # p = predicted_depth[depth_mask].unsqueeze(0).T
-                # predicted_depth[depth_mask] = (torch.cat((p, ones), dim=1) @ depth_trans.to(torch.float64)).squeeze(1)
-                # predicted_depth[~depth_mask] = new_max
-                # predicted_depth = torch.clamp(predicted_depth, new_min, new_max)  # truncate
+                # metrics_dict["depth_mse"] = torch.nn.functional.mse_loss(
+                #     ground_truth_depth[depth_mask], predicted_depth[depth_mask])
+                metrics_dict["distance_l1"] = torch.nn.functional.l1_loss(distance_gt, distance_pred)
 
-                metrics_dict["depth_mse"] = torch.nn.functional.mse_loss(
-                    ground_truth_depth[depth_mask], predicted_depth[depth_mask])
+                # normalise distance maps between (0, 1)
+                white, black = 1.0, 0.0
+                distance_gt_normalised = black + (white - black) * (new_max - distance_gt) / \
+                                         (new_max - new_min)
+                distance_pred_normalised = black + (white - black) * (new_max - distance_pred) / \
+                                           (new_max - new_min)
 
-                # depth_dir = os.path.join(output_dir, 'depth_maps/train')
-                # if not os.path.exists(depth_dir):
-                #     os.makedirs(depth_dir)
-                # plt.imsave(os.path.join(depth_dir, 'r_' + str(idx) + '_depth_gt.png'),
-                #            (1-ground_truth_depth).cpu().squeeze().numpy(), cmap='gray')
-                # plt.imsave(os.path.join(depth_dir, 'r_' + str(idx) + '_depth_pred.png'),
-                #            (1-predicted_depth).cpu().squeeze().numpy(), cmap='gray')
-                #
-                # # save rgb images
-                # rgb_img = images_dict['img'].cpu().numpy()
-                # rgb_dir = os.path.join(output_dir, 'rgb_images/train')
-                # if not os.path.exists(rgb_dir):
-                #     os.makedirs(rgb_dir)
-                # plt.imsave(os.path.join(rgb_dir, 'r_' + str(idx) + '.png'), rgb_img)
+                depth_dir = os.path.join(output_dir, 'depth_maps/test_NEW_sigma_25')
+                if not os.path.exists(depth_dir):
+                    os.makedirs(depth_dir)
+                plt.imsave(os.path.join(depth_dir, 'r_' + str(idx) + '_depth_gt.png'),
+                           distance_gt_normalised.cpu().squeeze().numpy(), cmap='gray')
+                plt.imsave(os.path.join(depth_dir, 'r_' + str(idx) + '_depth_pred.png'),
+                           distance_pred_normalised.cpu().squeeze().numpy(), cmap='gray')
+
+                # save rgb images
+                rgb_img = images_dict['img'].cpu().numpy()
+                rgb_dir = os.path.join(output_dir, 'rgb_images/test_NEW_sigma_5.0')
+                if not os.path.exists(rgb_dir):
+                    os.makedirs(rgb_dir)
+                plt.imsave(os.path.join(rgb_dir, 'r_' + str(idx) + '.png'), rgb_img)
 
                 metrics_dict_list.append(metrics_dict)
                 progress.advance(task)